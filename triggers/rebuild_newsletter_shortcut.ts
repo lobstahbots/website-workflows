50b5bd429725754ed6801689bd532ddcb1878186
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2024-10-18 21:06:30 -0400

    use slack-provided constants instead of hardcoding them

--- triggers/rebuild_newsletter_shortcut.ts
+++ triggers/rebuild_newsletter_shortcut.ts
@@ -1,16 +1,17 @@
 import { Trigger } from "deno-slack-api/types.ts";
+import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
 import RebuildNewsletter from "../workflows/rebuild_newsletter.ts";
 
 const rebuildNewsletterShortcut: Trigger<
   typeof RebuildNewsletter.definition
 > = {
-  type: "shortcut",
+  type: TriggerTypes.Shortcut,
   name: "Rebuild newsletter",
   description: "Rebuild website newsletter from notion",
   workflow: "#/workflows/rebuild_newsletter_workflow",
   inputs: {
     channel_id: {
-      value: "{{data.channel_id}}",
+      value: TriggerContextData.Shortcut.channel_id,
     },
   },
 };
--- triggers/revalidate_website_shortcut.ts
+++ triggers/revalidate_website_shortcut.ts
@@ -1,16 +1,17 @@
 import { Trigger } from "deno-slack-api/types.ts";
+import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
 import RevalidateWebsite from "../workflows/revalidate_website.ts";
 
 const revalidateWebsiteShortcut: Trigger<
   typeof RevalidateWebsite.definition
 > = {
-  type: "shortcut",
+  type: TriggerTypes.Shortcut,
   name: "Revalidate website",
   description: "Revalidate website The Blue Alliance Data",
   workflow: "#/workflows/revalidate_website_workflow",
   inputs: {
     channel_id: {
-      value: "{{data.channel_id}}",
+      value: TriggerContextData.Shortcut.channel_id,
     },
   },
 };
